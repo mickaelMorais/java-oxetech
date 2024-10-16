3fef295f50e32c80e49ee7328281d7b6be06d44d
mickael <110117622+mickaelMorais@users.noreply.github.com>
2024-10-16 11:07:33 -0300

    POO

--- modulo 1/projeto/Calculadora.java
+++ modulo 1/projeto/Calculadora.java
@@ -42,23 +42,4 @@ public class Calculadora{
 
         scan.close();
     }
-    
-}
-
-class Operadores{
-    public static double soma(double n1, double n2){
-        return n1 + n2;
-    }
-
-    public static double sub(double n1, double n2){
-        return n1 - n2;
-    }
-
-    public static double div(double n1, double n2){
-        return n1 / n2;
-    }
-
-    public static double mult(double n1, double n2){
-        return n1 * n2;
-    }
 }
---
+++ modulo 1/projeto/Operadores.java
@@ -0,0 +1,17 @@
+class Operadores{
+    public static double soma(double n1, double n2){
+        return n1 + n2;
+    }
+
+    public static double sub(double n1, double n2){
+        return n1 - n2;
+    }
+
+    public static double div(double n1, double n2){
+        return n1 / n2;
+    }
+
+    public static double mult(double n1, double n2){
+        return n1 * n2;
+    }
+}
